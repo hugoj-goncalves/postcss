fdda17cdf346e460247e543553551de15a104785
hugoj-goncalves <hugo159@gmail.com>
2024-12-12 15:31:50 -0300

    WIP

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -rf dist
-npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.esm.json --format=esm --outfile=./dist/lib/postcss.js
-npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.esm.json --format=esm --outfile=./dist/lib/postcss.njs
-npx tsc -p ./tsconfig.esm.json
+# npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.esm.json --format=esm --outfile=./dist/lib/postcss.js
+# npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.esm.json --format=esm --outfile=./dist/lib/postcss.njs
+# npx tsc -p ./tsconfig.esm.json
+mkdir -p ./dist/lib
+cp ./lib/* ./dist/lib/
 cp package.json dist/

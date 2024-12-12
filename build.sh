0334ba8699c15f23db09c84b12734459dcabee52
hugoj-goncalves <hugo159@gmail.com>
2024-12-12 15:17:32 -0300

    WIP

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf dist
-npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.json --format=esm --outfile=./dist/lib/postcss.js
-npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.json --format=esm --outfile=./dist/lib/postcss.njs
+npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.esm.json --format=esm --outfile=./dist/lib/postcss.js
+npx esbuild ./lib/postcss.mjs --bundle --platform=neutral --tsconfig=./tsconfig.esm.json --format=esm --outfile=./dist/lib/postcss.njs
+npx tsc -p ./tsconfig.esm.json
 cp package.json dist/
